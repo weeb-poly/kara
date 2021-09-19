ed7f18a0ab20105ed4fa0308e62b2370064bc84c
Timothy Cyrus <tcyrus@tcyr.us>
2021-09-19 18:12:04 -0400

    Update validate.js
    Still untested, but it might work

--- tools/validate.js
+++ tools/validate.js
@@ -1,12 +1,15 @@
 import fs from "fs";
+import path from "path";
 
 import Ajv from "ajv";
 import addFormats from "ajv-formats";
+import addSemver from "ajv-semver";
 
 import addCustomFormats from "./formats";
 
 const ajv = new Ajv();
 addFormats(ajv);
+addSemver(ajv);
 addCustomFormats(ajv);
 
 function readJsonFile(filename) {
@@ -21,4 +24,34 @@ const tagFileSchema = readJsonFile('./schema/karafile.json');
 const karaFileValidator = ajv.compile(karaFileSchema);
 const tagFileValidator = ajv.compile(tagFileSchema);
 
-// TODO: Loop through dirs and get all tag and karaoke filepaths
+function allFilesInDir(dir) {
+    let files = fs.readdirSync(dir);
+  
+    files = files.map(file => path.join(dir, file));
+    files = files.filter(file => {
+        const stat = fs.statSync(file);
+        return stat.isFile();
+    });
+    
+    return files;
+}
+
+const tagFiles = allFilesInDir('../tags/');
+const karaFiles = allFilesInDir('../karaokes/');
+
+function validateType(schemaValidator, files) {
+    for (const file of files) {
+        let rawdata = fs.readFileSync(filename);
+        let data = JSON.parse(rawdata);
+        let res = schemaValidator(data);
+        if (!res) {
+            console.error('Validation error:', file, schemaValidator.error);
+            return false
+        }
+    }
+
+    return true;
+}
+
+validateType(tagFileValidator, tagFiles);
+validateType(karaFileValidator, karaFiles);
